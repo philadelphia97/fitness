33f797589e55205c98fdc3309d95feb43936bf5f
jordan <jordansteger95@gmail.com>
2022-06-25 09:50:14 -0500

    second commit

--- api/users.js
+++ api/users.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const { createUser, getUserByUsername, getUserById } = require('./users')
+const { userTakenError, PasswordTooShortError } = require('../errors')
 
 // POST /api/users/login
 
 // POST /api/users/register
+
+router.post("/register", async (req, res, next) => {
+    const { username, password } = req.body;
+  
+    try {
+      const _user = await getUserByUsername(username);
+  
+      if (_user) {
+        next(userTakenError);
+      }
+  
+      const user = await createUser({
+        username,
+        password
+      });
+  
+      res.send({
+        user
+      });
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
 
 // GET /api/users/me
 
--- db/users.js
+++ db/users.js
@@ -26,16 +26,68 @@ async function createUser({ username, password }) {
     throw error;
   }
 }
+async function getUser({ username, password }) {
+  try {
+    getUserByUsername()
+    const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
+    const match = await bcrypt.compare(password, hashedPassword)
+    if(match) {
+      return username
+    }
+    if (!match) {
+      return null
+    }
 
-// async function getUser({ username, password }) {}
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-async function getUserById(userId) {}
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+      SELECT id, username, password
+      FROM users
+      WHERE id=${userId}
+    `);
 
-// async function getUserByUsername(userName) {}
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  } catch (error) {
+    console.error(error)
+    throw error;
+  }
+}
+
+async function getUserByUsername(userName) {
+    try {
+      const {
+        rows: [user],
+      } = await client.query(
+        `
+        SELECT *
+        FROM users
+        WHERE username=$1;
+      `,
+        [userName]
+      );
+  
+      return user;
+    } catch (error) {
+      console.error(error)
+      throw error;
+    }
+  }
+
 
 module.exports = {
   createUser,
-  // getUser,
-  // getUserById,
-  // getUserByUsername,
+  getUser,
+  getUserById,
+  getUserByUsername,
 };
